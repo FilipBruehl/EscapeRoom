dc273ab66163380f85aeaf690c8ad37d27dada57
Filip Brühl <filip.h.bruehl@student.fh-kiel.de>
2018-12-17 03:58:59 +0100

    4am
    Worked till 4 am

---
+++ Assets/Inventory.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inventory : MonoBehaviour {
+    public GameObject[] items = new GameObject[5];
+    public const int size = 5;
+
+    void Start()
+    {
+
+    }
+
+    public void AddItem(GameObject newItem)
+    {
+        for (int i = 0; i < size; i++)
+        {
+            if (items[i] == null)
+            {
+                items[i] = newItem;
+                Debug.Log(newItem.name + " addet to inventory");
+            }
+        }
+    }
+
+    public void DeleteItem(GameObject removeItem)
+    {
+        for(int i = 0; i < size; i++)
+        {
+            if (items[i] == removeItem)
+            {
+                items[i] = null;
+            }
+        }
+    }
+
+    public bool DeleteItem(int pos)
+    {
+        if(pos > size)
+        {
+            return false;
+        } else
+        {
+            items[pos] = null;
+            return true;
+        }
+    }
+
+    public bool HasItem(GameObject other)
+    {
+        foreach(GameObject item in items)
+        {
+            if(item == other)
+            {
+                return true;
+            }
+            return false;
+        }
+        return false;
+    }
+
+    public bool HasItem(string name)
+    {
+        foreach(GameObject item in items)
+        {
+            if(item.name == name || item.tag == name)
+            {
+                return true;
+            }
+            return false;
+        }
+        return false;
+    }
+
+}
