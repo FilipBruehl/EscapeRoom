994adc49394ae13dc36da6019bc2b0facc80a10a
Filip Hugo Brühl <filip.h.bruehl@student.fh-kiel.de>
2018-12-28 19:31:03 +0100

    LastLevel & Sounds
    - LastLevel erstellt
    - Test Sounds hinzugefügt und getestet

--- Assets/Door.cs
+++ Assets/Door.cs
@@ -9,6 +9,9 @@ public class Door : VRTK_InteractableObject
     public GameObject key;
     public GameObject inventory;
     public GameObject light;
+    public GameObject speakers;
+    public AudioClip succes;
+    public AudioClip fail;
     public string nextSceneName;
 
 	// Use this for initialization
@@ -22,10 +25,12 @@ public class Door : VRTK_InteractableObject
         {
             gameObject.GetComponent<Door>().touchHighlightColor = Color.green;
             light.GetComponentInChildren<Light>().color = Color.green;
+            speakers.GetComponent<AudioSource>().clip = succes;
         } else
         {
             gameObject.GetComponent<Door>().touchHighlightColor = Color.red;
             light.GetComponentInChildren<Light>().color = Color.red;
+            speakers.GetComponent<AudioSource>().clip = fail;
         }
 	}
 
@@ -33,6 +38,7 @@ public class Door : VRTK_InteractableObject
     {
         base.StartUsing(currentUsingObject);
         Debug.Log(gameObject.name + " start using");
+        speakers.GetComponent<AudioSource>().Play();
         if(GameObject.Find(key.name).GetComponent<Key>().IsGrabbed() || inventory.GetComponent<Inventory>().HasItem(key)) {
             Debug.Log("Won");
             if (nextSceneName.Length > 0)
---
+++ Assets/DoorLastLevel.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using VRTK;
+
+public class DoorLastLevel : VRTK_InteractableObject
+{
+    public GameObject light;
+    public GameObject speakers;
+    public AudioClip fail;
+
+    // Use this for initialization
+    void Start()
+    {
+        gameObject.GetComponent<DoorLastLevel>().touchHighlightColor = Color.red;
+        light.GetComponentInChildren<Light>().color = Color.red;
+        speakers.GetComponent<AudioSource>().clip = fail;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public override void StartUsing(VRTK_InteractUse currentUsingObject = null)
+    {
+        base.StartUsing(currentUsingObject);
+        Debug.Log(gameObject.name + " start using");
+        speakers.GetComponent<AudioSource>().Play();
+    }
+}
